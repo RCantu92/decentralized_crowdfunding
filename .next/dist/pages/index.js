45514fdba5a943233658c7469a3a337c13ea6f01
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-04-23 12:49:19 -0500

    Change newcampaign.js to index.js

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/robertocantu/udemy_eth_course/decentralized_crowd_funding/pages/index.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, 'This is the campaign list page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQU8sY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFBUCxBQUFPLEFBQ1Y7QUFGRCIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvcm9iZXJ0b2NhbnR1L3VkZW15X2V0aF9jb3Vyc2UvZGVjZW50cmFsaXplZF9jcm93ZF9mdW5kaW5nIn0=

bec63f7c8dfc503a9f3ad27f0d39068113ffc96a
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-04-28 13:32:45 -0500

    Render <Button> before this.renderCampaigns() in index.js

--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,13 @@ class CampaignIndex extends Component {
                 <div>
                     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
                     <h3>Open Campaigns</h3>
-                    {this.renderCampaigns()}
-                    <Button 
+                    <Button
+                        floated="right"
                         content="Create Campaign"
                         icon="add circle"
                         primary
                     />
+                    {this.renderCampaigns()}
                 </div>
             </Layout>
         );
